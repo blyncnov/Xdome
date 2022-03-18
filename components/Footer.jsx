49c7a0b29fd75a7589480f53e62dddbaca2c4558
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-03-18 06:54:47 -0700

    xdome v6

---
+++ components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+import { Container, Card, Flexbox, Restrainer, Logo, Circle } from "../styles/constants/Constants"
+
+
+const Footer = () => {
+    return (
+        <>
+            <Container Bg="#eee" style={{ borderRadius: "10% 5% 0% 0 ", padding: "1em 0" }}>
+                <Restrainer style={{ textAlign: "center" }}>
+
+
+                    <h4> Xdome</h4>
+                    <h6>Best ToDo application by blyncnov</h6>
+
+
+                </Restrainer>
+            </Container>
+
+        </>
+    )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 
 import Homepage from "../components/Homepage.jsx"
 import Header from '../components/Header.jsx'
+import Footer from '../components/Footer.jsx'
 
 export default function Home() {
   return (
@@ -12,7 +13,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <Header/>
-    <Homepage/>
+      <Homepage />
+      <Footer/>
     </>
   )
 }
